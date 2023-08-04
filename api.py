7b5cc8a20c8890b07e727a88b6eacc51b1b87591
Suliot <suliot@msn.com>
2023-08-04 16:03:52 -0700

    Standardize

--- api.py
+++ api.py
@@ -4,14 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 hf_api_key = os.getenv('HUGGINGFACEHUB_API_TOKEN') # api key for huggingface.co in .env file
 model_name = os.getenv('MODEL_NAME') # model name for huggingface.co in .env file
+api_url = f"https://api-inference.huggingface.co/models/{model_name}"
 
 
-def set_local_vars():
-    api_url = f"https://api-inference.huggingface.co/models/{model_name}"
-    return api_url
-
-
-def hf_api(api_url, text):
+def hf_api(text):
     headers = {"Authorization": f"Bearer {hf_api_key}"}
 
     def query(payload):
@@ -25,9 +21,8 @@ def hf_api(api_url, text):
 
 
 def main():
-    api_url = set_local_vars()
     text = "paragraph about artificial intelligence"
-    return_value = hf_api(api_url, text)
+    return_value = hf_api(text)
     print(return_value)
 
 
